b3f3c4e2d576cd8ea536a1a536bc41a53d6e7abd
mustafaba0 <91112982+mustafaba0@users.noreply.github.com>
2022-03-21 10:05:21 +0300

    Add files via upload

---
+++ KutuphaneTakip/AccessDB.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+namespace KutuphaneTakip
+{
+    partial class AccessDB
+    {
+        Dictionary<string, string> tables = new Dictionary<string, string>()
+        {
+            { "user", "Kullanici" },
+            { "books", "Kitaplar" }
+        };
+        OleDbConnection connection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=db.accdb");
+
+        public void yeniTabloKaydet(string key, string table)
+        {
+            tables[key] = table;
+        }
+
+
+
+    }
+}
